d9cbef6c2211021e244d46774cf673bb93a86511
lesoaress <lemayara16@gmail.com>
2019-05-21 15:33:03 -0300

    exemplos aula 9 com while

--- desafioAula7Raiz.py
+++ desafioAula7Raiz.py
@@ -9,12 +9,12 @@ delta = b**2 - 4*a*c
 if delta < 0:
     print("Não existe raiz real para essa expressão!")
 else:
+    raiz = (-b + math.sqrt(delta))/(2*a)
+    raiz2 = (-b - math.sqrt(delta))/(2*a)
     if delta == 0:
-        raiz = (-b + math.sqrt(delta))/(2*a)
         print("A única raiz é: ", raiz)
     else:
-        raiz = (-b + math.sqrt(delta))/(2*a)
-        raiz2 = (-b - math.sqrt(delta))/(2*a)
         print("Raiz(es) = ", raiz, "e ", raiz2)
+        
 
     
---
+++ exAula9.py
@@ -0,0 +1,6 @@
+i = 0
+qtde_de_potencias = int(input("Digite quantas potências você quer ver: "))
+
+while i <= qtde_de_potencias:
+    print(2**i)
+    i = i + 1
