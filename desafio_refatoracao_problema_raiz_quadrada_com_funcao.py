ccc3908b32db1e4eff9914d5ae08884d119bb717
lesoaress <lemayara16@gmail.com>
2019-05-27 20:32:15 -0300

    refatoracao raiz quadrada

--- desafio_refatoracao_problema_raiz_quadrada_com_funcao.py
+++ desafio_refatoracao_problema_raiz_quadrada_com_funcao.py
@@ -1,8 +1,7 @@
 import math
 
 def calcula_delta(a, b, c):
-    delta = b**2 - 4*a*c
-    return delta
+    return b**2 - 4*a*c
 
 def calcula_raizes(a, b, delta):
     raiz = (-b + math.sqrt(delta))/(2*a)
@@ -10,19 +9,22 @@ def calcula_raizes(a, b, delta):
     return raiz, raiz2
     
 def calcula_funcao(a, b, c):
-    if calcula_delta(a, b, c) < 0:
-        return "Não existe raiz real para essa expressão!"
+    delta = calcula_delta(a, b, c)
+    if delta < 0:
+       print("Não existe raiz real para essa expressão!")
     else:
-        if calcula_delta(a, b, c) == 0:
-            return "A única raiz é: ", calcula_raizes(a, b, calcula_delta(a, b, c))
+        if delta == 0:
+            print("A única raiz é: ", calcula_raizes(a, b, delta))
         else:
-            return "Raiz(es) = ", calcula_raizes(a, b, calcula_delta(a, b, c))
+            print("Raiz(es) = ", calcula_raizes(a, b, delta))
 
-a = int(input("Insira o 'a' da equação: "))
-b = int(input("Insira o 'b' da equação: "))
-c = int(input("Insira o 'c' da equação: "))
+def main():
+    a_digitado = int(input("Insira o 'a' da equação: "))
+    b_digitado = int(input("Insira o 'b' da equação: "))
+    c_digitado = int(input("Insira o 'c' da equação: "))
+    calcula_funcao(a_digitado,b_digitado,c_digitado)
 
-print(calcula_funcao(a, b, c))
+
 
 
 
